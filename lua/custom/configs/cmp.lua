37a9690baf61158210d4a2009f21d1b19611debc
Anh Dau <redbeanboy98@gmail.com>
2023-11-16 22:33:28 +0700

    chore(cmp): replicate old <Tab> and <S-tab> behavior
    v2.5.2

--- lua/custom/configs/cmp.lua
+++ lua/custom/configs/cmp.lua
@@ -21,8 +21,32 @@ local opts = vim.tbl_deep_extend("force", default_opts, {
 
 -- override the whole mapping table
 opts.mapping = {
-  ["<C-k>"] = cmp.mapping.select_prev_item(),
-  ["<C-j>"] = cmp.mapping.select_next_item(),
+  -- select previous item
+  ["<C-k>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item()
+    elseif require("luasnip").jumpable(-1) then
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+    else
+      fallback()
+    end
+  end, {
+    "i",
+    "s",
+  }),
+  -- select next item
+  ["<C-j>"] = cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_next_item()
+    elseif require("luasnip").expand_or_jumpable() then
+      vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+    else
+      fallback()
+    end
+  end, {
+    "i",
+    "s",
+  }),
   ["<C-u>"] = cmp.mapping.scroll_docs(-4),
   ["<C-d>"] = cmp.mapping.scroll_docs(4),
   ["<C-e>"] = cmp.mapping.close(),
