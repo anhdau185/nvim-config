8529c72b7cb960d1831c04fd2d7fe48685eb8eaf
Anh Dau <redbeanboy98@gmail.com>
2023-11-25 09:09:12 +0700

    chore(telescope): update naming

--- lua/custom/configs/telescope.lua
+++ lua/custom/configs/telescope.lua
@@ -1,11 +1,12 @@
-local default_opts = require "plugins.configs.telescope"
 local actions = require "telescope.actions"
+local default_opts = require "plugins.configs.telescope"
 
-local EXCLUDE_PATTERN = "**/{.git,node_modules}/**"
-local RG_GLOB_PATTERN = "!" .. EXCLUDE_PATTERN
+local EXCLUDE_GLOB_PATTERN = "**/{.git,node_modules}/**"
+local RG_GLOB_PATTERN = "!" .. EXCLUDE_GLOB_PATTERN
+local RG_DEFAULT_ARGS = " -. -L -g " .. RG_GLOB_PATTERN .. " " -- equivalent to: --hidden --follow --glob !**/{.git,node_modules}/**
 
 -- all defaults: https://github.com/nvim-telescope/telescope.nvim/blob/master/lua/telescope/mappings.lua#L133
-local mappings = {
+local common_mappings = {
   ["<C-k>"] = actions.move_selection_previous,
   ["<C-j>"] = actions.move_selection_next,
   ["<C-h>"] = actions.preview_scrolling_left,
@@ -14,19 +15,23 @@ local mappings = {
 
   -- Switch to live_grep_args retaining the current prompt
   ["<C-g>"] = function(prompt_bufnr)
-    local current_picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-    local prompt = vim.trim(current_picker:_get_prompt())
+    local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+    local prompt = vim.trim(picker:_get_prompt())
 
     if prompt == "" then
       return
     end
 
-    local rg_args = " -. -L -g " .. RG_GLOB_PATTERN .. " " -- equivalent to: --hidden --follow --glob "pattern"
-    prompt = require("telescope-live-grep-args.helpers").quote(prompt, { quote_char = '"' })
-    prompt = prompt .. rg_args
+    local rg_search_pattern =
+      require("telescope-live-grep-args.helpers").quote(prompt, { -- pattern should be quoted in case of whitespace
+        quote_char = '"',
+      })
+    local new_prompt = rg_search_pattern .. RG_DEFAULT_ARGS
 
     vim.schedule(function()
-      require("telescope").extensions.live_grep_args.live_grep_args { default_text = prompt }
+      require("telescope").extensions.live_grep_args.live_grep_args {
+        default_text = new_prompt,
+      }
     end)
     actions.close(prompt_bufnr)
   end,
@@ -44,14 +49,14 @@ local opts = vim.tbl_deep_extend("force", default_opts, {
     wrap_results = true,
     prompt_prefix = "", -- no prefix icon
     mappings = {
-      i = mappings,
-      n = mappings,
+      i = common_mappings,
+      n = common_mappings,
     },
   },
 
   pickers = {
     find_files = {
-      find_command = { "fd", "--type", "file", "--hidden", "--follow", "--exclude", EXCLUDE_PATTERN },
+      find_command = { "fd", "--type", "file", "--hidden", "--follow", "--exclude", EXCLUDE_GLOB_PATTERN },
     },
     live_grep = {
       additional_args = { "--hidden", "--follow" },
@@ -62,7 +67,7 @@ local opts = vim.tbl_deep_extend("force", default_opts, {
   },
 })
 
--- in order to load_extension this at lua/plugins/init.lua:247
+-- in order to for this extension to be loaded at lua/plugins/init.lua:247
 table.insert(opts.extensions_list, "live_grep_args")
 
 return opts
