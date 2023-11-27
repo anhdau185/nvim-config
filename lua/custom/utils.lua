efb9a68afc5f0df5fcc623cd5928e0194eeeeb5b
Anh Dau <redbeanboy98@gmail.com>
2023-11-27 14:49:26 +0700

    feat(utils): add util fn to get visual selection

--- lua/custom/utils.lua
+++ lua/custom/utils.lua
@@ -51,4 +51,16 @@ M.rg_exclude_glob = function()
   return string.format("!**/{%s}/**", exclude)
 end
 
+M.get_visual_selection = function()
+  vim.cmd 'noau normal! "vy"'
+  local text = vim.fn.getreg "v"
+  vim.fn.setreg("v", {})
+  text = string.gsub(text, "\n", "")
+
+  if #text > 0 then
+    return text
+  end
+  return ""
+end
+
 return M
